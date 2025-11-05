b91abd4c585de17318d9e5ec9fb0109ef4fe95fb
David <David.Nabel-Moris@etu.univ-smb.fr>
2025-11-05 12:19:16 +0100

    test

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
 
 
 
 int main(){
-    printf("sdjbi");
+    node *t5 = create_feuille(5, NULL);
+    node *t6 = create_feuille(8, NULL);
+
+    afficher_feuille(t5);
+    afficher_feuille(t6);
+    node *n = create_node(10, t5, t6, NULL);
+    afficher_node(n);
     return 0;
 }
--- node.c
+++ node.c
@@ -1,21 +1,23 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "node.h"
 
-void create_feuille(int racine, int *tab){
+node* create_feuille(int racine, int *tab){
     node* feuille = (node*)malloc(sizeof(node));
     if (feuille == NULL) {
-        return;
+        return NULL;
     }
     feuille->racine = racine;
     feuille->gauche = NULL;
     feuille->droite = NULL;
     feuille->tab = tab;
-    return feuille
+    return feuille;
 }
-void create_node(int racine, node *gauche, node *droite, int* tab){
+
+node* create_node(int racine, node *gauche, node *droite, int* tab){
     node* new_node = (node*)malloc(sizeof(node));
     if (new_node == NULL) {
-        return;
+        return NULL;
     }
     new_node->racine = racine;
     new_node->gauche = gauche;
@@ -24,4 +26,25 @@ void create_node(int racine, node *gauche, node *droite, int* tab){
     return new_node;
 }
 
-
+void afficher_feuille(node *arbre){
+    if (arbre == NULL) {
+        return;
+    }
+    printf("Feuille racine: %d\n", arbre->racine);
+}
+void afficher_node(node *arbre){
+    if (arbre == NULL) {
+        return;
+    }
+    printf("Node racine: %d\n", arbre->racine);
+    if (arbre->gauche != NULL) {
+        printf("Gauche racine: %d\n", arbre->gauche->racine);
+    } else {
+        printf("Gauche: NULL\n");
+    }
+    if (arbre->droite != NULL) {
+        printf("Droite racine: %d\n", arbre->droite->racine);
+    } else {
+        printf("Droite: NULL\n");
+    }
+}
