9453c9d1797e4330a5003a1cd07591079fc0b693
Ton Nom <jfjjfjfdjj@gmail.com>
2025-11-05 12:30:00 +0100

    la sebon

---
+++ node.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "node.h"
+#define TAB_MAX 100
+
+node* create_feuille(int racine, int *tab){
+    node* feuille = (node*)malloc(sizeof(node));
+    if (feuille == NULL) {
+        return NULL;
+    }
+    feuille->racine = racine;
+    feuille->gauche = NULL;
+    feuille->droite = NULL;
+    feuille->tab = tab;
+    return feuille;
+}
+
+node* create_node(int racine, node *gauche, node *droite, int* tab){
+    node* new_node = (node*)malloc(sizeof(node));
+    if (new_node == NULL) {
+        return NULL;
+    }
+    new_node->racine = racine;
+    new_node->gauche = gauche;
+    new_node->droite = droite;
+    new_node->tab = tab;
+    return new_node;
+}
+
+void ajt_element_fin(int **tab, int element) {
+    int *temp = *tab;
+
+    while (*temp != NULL) {
+        temp++;
+    }
+    *temp = element;
+    *(temp + 1) = NULL; 
+}
+void afficher_feuille(node *arbre){
+    if (arbre == NULL) {
+        return;
+    }
+    printf("Feuille racine: %d\n", arbre->racine);
+}
+void afficher_node(node *arbre){
+    if (arbre == NULL) {
+        return;
+    }
+    printf("Node racine: %d\n", arbre->racine);
+    if (arbre->gauche != NULL) {
+        printf("Gauche racine: %d\n", arbre->gauche->racine);
+    } else {
+        printf("Gauche: NULL\n");
+    }
+    if (arbre->droite != NULL) {
+        printf("Droite racine: %d\n", arbre->droite->racine);
+    } else {
+        printf("Droite: NULL\n");
+    }
+}
+
