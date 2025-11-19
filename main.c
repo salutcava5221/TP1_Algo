21c453cbf37d83acb37930ce72f305ba866a3a5b
Ton Nom <jfjjfjfdjj@gmail.com>
2025-11-19 08:55:00 +0100

    change fonction affiche

--- main.c
+++ main.c
@@ -8,8 +8,9 @@ int main(){
     node *t5 = create_feuille(5, NULL);
     node *t6 = create_feuille(8, NULL);
 
-    afficher_feuille(t5);
-    afficher_feuille(t6);
+    //afficher_feuille(t5);
+    //afficher_feuille(t6);
+
     node *n = create_node(10, t5, t6, NULL);
     afficher_node(n);
     return 0;
