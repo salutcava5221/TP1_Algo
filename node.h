6fc46058cd6cfdcf0d0379403e5d50527c668b1f
salutcava5221 <jfjjfjfdjj@gmail.com>
2025-11-19 09:46:42 +0100

    Update node.h

--- node.h
+++ node.h
@@ -1,9 +1,19 @@
+#define TAB_MAX 100
+
 typedef struct node{
     int racine;
     struct node* gauche;
     struct node* droite;
     int *tab;
 }node;
+
+
+typedef struct {
+    int data[TAB_MAX];   // stockage
+    int taille;          // nb d’éléments utilisés
+} Tableau;
+
+
 
 node* create_feuille(int racine, int *tab);
 
@@ -17,9 +27,11 @@ void enveler_racine_node(node *arbre, int **tab);
 
 void afficher_node(node *arbre);
 
-void final(node *arbre,int **tab);
+void afficher_tableau(Tableau *t);
 
+void init_tableau(Tableau *t);
 
+void ajouter_fin(Tableau *t, int valeur);
 
 
 
